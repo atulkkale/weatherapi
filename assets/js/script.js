93abee8122adce6cac2fde29714c8f4379b380cd
Atul Kale <kaleatul9@gmail.com>
2021-01-27 11:20:51 +0530

    css changes implemented

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,11 @@
 /* Author: 
+api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
 
+5a23d8283cd00609e5130d95f233af52
 */
+
+// fetch(api.openweathermap.org/data/2.5/weather?q={"Mumbai"}&appid={"5a23d8283cd00609e5130d95f233af52"})
+//   .then(res = function(){console.log(res)})
 
 
 
